37250816a146cfe7b94ab418f86162e7a017dd7b
Lenka Verzichová <lenkaverzichova@gmail.com>
2021-05-29 15:59:14 +0200

    Add Component MainMap

---
+++ src/Game/components/Levels/levels.js
@@ -0,0 +1,485 @@
+export const levels = [
+  {
+    tiles: [
+      {
+        style: {
+          top: '287px',
+          left: '30px',
+          transform: 'rotate(0deg)',
+        },
+        showDelay: '0s',
+      },
+    ],
+    button: {
+      style: {
+        top: '268px',
+        left: '75px',
+      },
+      showDelay: '200ms',
+    },
+  },
+  {
+    tiles: [
+      {
+        style: {
+          top: '274px',
+          left: '132px',
+          transform: 'rotate(-20deg)',
+        },
+        showDelay: '0s',
+      },
+      {
+        style: {
+          top: '262px',
+          left: '171px',
+          transform: 'rotate(-15deg)',
+        },
+        showDelay: '200ms',
+      },
+      {
+        style: {
+          top: '255px',
+          left: '212px',
+          transform: 'rotate(-5deg)',
+        },
+        showDelay: '400ms',
+      },
+      {
+        style: {
+          top: '253px',
+          left: '252px',
+          transform: 'rotate(-2deg)',
+        },
+        showDelay: '600ms',
+      },
+    ],
+    button: {
+      style: {
+        top: '230px',
+        left: '296px',
+      },
+      showDelay: '800ms',
+    },
+  },
+  {
+    tiles: [
+      {
+        style: {
+          top: '237px',
+          left: '354px',
+          transform: 'rotate(-20deg)',
+        },
+        showDelay: '0s',
+      },
+      {
+        style: {
+          top: '223px',
+          left: '393px',
+          transform: 'rotate(-20deg)',
+        },
+        showDelay: '200ms',
+      },
+
+      {
+        style: {
+          top: '211px',
+          left: '433px',
+          transform: 'rotate(-15deg)',
+        },
+        showDelay: '400ms',
+      },
+      {
+        style: {
+          top: '203px',
+          left: '473px',
+          transform: 'rotate(-10deg)',
+        },
+        showDelay: '600ms',
+      },
+    ],
+    button: {
+      style: {
+        top: '177px',
+        left: '517px',
+      },
+      showDelay: '800ms',
+    },
+  },
+  {
+    tiles: [
+      {
+        style: {
+          top: '200px',
+          left: '581px',
+          transform: 'rotate(3deg)',
+        },
+        showDelay: '0s',
+      },
+      {
+        style: {
+          top: '203px',
+          left: '622px',
+          transform: 'rotate(3deg)',
+        },
+        showDelay: '200ms',
+      },
+      {
+        style: {
+          top: '206px',
+          left: '662px',
+          transform: 'rotate(-1deg)',
+        },
+        showDelay: '400ms',
+      },
+      {
+        style: {
+          top: '202px',
+          left: '702px',
+          transform: 'rotate(-13deg)',
+        },
+        showDelay: '600ms',
+      },
+    ],
+
+    button: {
+      style: {
+        top: '170px',
+        left: '747px',
+      },
+      showDelay: '800ms',
+    },
+  },
+  {
+    tiles: [
+      {
+        style: {
+          top: '186px',
+          left: '810px',
+          transform: 'rotate(-6deg)',
+        },
+        showDelay: '0s',
+      },
+      {
+        style: {
+          top: '187px',
+          left: '852px',
+          transform: 'rotate(6deg)',
+        },
+        showDelay: '200ms',
+      },
+      {
+        style: {
+          top: '194px',
+          left: '893px',
+          transform: 'rotate(6deg)',
+        },
+        showDelay: '400ms',
+      },
+      {
+        style: {
+          top: '197px',
+          left: '933px',
+          transform: 'rotate(2deg)',
+        },
+        showDelay: '600ms',
+      },
+    ],
+
+    button: {
+      style: {
+        top: '178px',
+        left: '980px',
+      },
+      showDelay: '800ms',
+    },
+  },
+  {
+    tiles: [
+      {
+        style: {
+          top: '208px',
+          left: '1047px',
+          transform: 'rotate(12deg)',
+        },
+        showDelay: '0s',
+      },
+      {
+        style: {
+          top: '223px',
+          left: '1088px',
+          transform: 'rotate(25deg)',
+        },
+        showDelay: '200ms',
+      },
+      {
+        style: {
+          top: '246px',
+          left: '1124px',
+          transform: 'rotate(38deg)',
+        },
+        showDelay: '400ms',
+      },
+      {
+        style: {
+          top: '273px',
+          left: '1154px',
+          transform: 'rotate(42deg)',
+        },
+        showDelay: '600ms',
+      },
+    ],
+
+    button: {
+      style: {
+        top: '291px',
+        left: '1178px',
+      },
+      showDelay: '800ms',
+    },
+  },
+  {
+    tiles: [
+      {
+        style: {
+          top: '347px',
+          left: '1209px',
+          transform: 'rotate(68deg)',
+        },
+        showDelay: '0s',
+      },
+      {
+        style: {
+          top: '385px',
+          left: '1217px',
+          transform: 'rotate(84deg)',
+        },
+        showDelay: '200ms',
+      },
+      {
+        style: {
+          top: '419px',
+          left: '1199px',
+          transform: 'rotate(-33deg)',
+        },
+        showDelay: '400ms',
+      },
+      {
+        style: {
+          top: '432px',
+          left: '1162px',
+          transform: 'rotate(-10deg)',
+        },
+        showDelay: '600ms',
+      },
+      {
+        style: {
+          top: '433px',
+          left: '1109px',
+          transform: 'rotate(5deg)',
+        },
+        showDelay: '800ms',
+      },
+    ],
+
+    button: {
+      style: {
+        top: '408px',
+        left: '1044px',
+      },
+      showDelay: '1000ms',
+    },
+  },
+  {
+    tiles: [
+      {
+        style: {
+          top: '423px',
+          left: '1002px',
+          transform: 'rotate(8deg)',
+        },
+        showDelay: '0s',
+      },
+      {
+        style: {
+          top: '416px',
+          left: '960px',
+          transform: 'rotate(6deg)',
+        },
+        showDelay: '200ms',
+      },
+      {
+        style: {
+          top: '414px',
+          left: '917px',
+          transform: 'rotate(-2deg)',
+        },
+        showDelay: '400ms',
+      },
+      {
+        style: {
+          top: '418px',
+          left: '873px',
+          transform: 'rotate(-7deg)',
+        },
+        showDelay: '600ms',
+      },
+    ],
+
+    button: {
+      style: {
+        top: '403px',
+        left: '807px',
+      },
+      showDelay: '800ms',
+    },
+  },
+  {
+    tiles: [
+      {
+        style: {
+          top: '435px',
+          left: '759px',
+          transform: 'rotate(-15deg)',
+        },
+        showDelay: '0s',
+      },
+      {
+        style: {
+          top: '444px',
+          left: '719px',
+          transform: 'rotate(-13deg)',
+        },
+        showDelay: '200ms',
+      },
+      {
+        style: {
+          top: '448px',
+          left: '679px',
+          transform: 'rotate(-2deg)',
+        },
+        showDelay: '400ms',
+      },
+      {
+        style: {
+          top: '445px',
+          left: '639px',
+          transform: 'rotate(6deg)',
+        },
+        showDelay: '600ms',
+      },
+      {
+        style: {
+          top: '438px',
+          left: '598px',
+          transform: 'rotate(8deg)',
+        },
+        showDelay: '800ms',
+      },
+    ],
+
+    button: {
+      style: {
+        top: '410px',
+        left: '530px',
+      },
+      showDelay: '1000ms',
+    },
+  },
+
+  {
+    tiles: [
+      {
+        style: {
+          top: '432px',
+          left: '483px',
+          transform: 'rotate(-9deg)',
+        },
+        showDelay: '0s',
+      },
+      {
+        style: {
+          top: '439px',
+          left: '443px',
+          transform: 'rotate(-12deg)',
+        },
+        showDelay: '200ms',
+      },
+      {
+        style: {
+          top: '442px',
+          left: '403px',
+          transform: 'rotate(-2deg)',
+        },
+        showDelay: '400ms',
+      },
+      {
+        style: {
+          top: '438px',
+          left: '363px',
+          transform: 'rotate(9deg)',
+        },
+        showDelay: '600ms',
+      },
+      {
+        style: {
+          top: '430px',
+          left: '323px',
+          transform: 'rotate(7deg)',
+        },
+        showDelay: '800ms',
+      },
+    ],
+
+    button: {
+      style: {
+        top: '402px',
+        left: '258px',
+      },
+      showDelay: '1000ms',
+    },
+  },
+  {
+    tiles: [
+      {
+        style: {
+          top: '425px',
+          left: '214px',
+          transform: 'rotate(-5deg)',
+        },
+        showDelay: '0s',
+      },
+      {
+        style: {
+          top: '424px',
+          left: '174px',
+          transform: 'rotate(2deg)',
+        },
+        showDelay: '200ms',
+      },
+      {
+        style: {
+          top: '422px',
+          left: '134px',
+          transform: 'rotate(-2deg)',
+        },
+        showDelay: '400ms',
+      },
+      {
+        style: {
+          top: '425px',
+          left: '94px',
+          transform: 'rotate(-8deg)',
+        },
+        showDelay: '600ms',
+      },
+    ],
+
+    button: {
+      style: {
+        top: '385px',
+        left: '26px',
+      },
+      showDelay: '800ms',
+    },
+  },
+];
---
+++ src/Game/components/MainMap/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useAuth } from './../../../Auth/AuthContext';
+import Map from './../../../images/map.png';
+import './style.css';
+
+export const MainMap = () => {
+  const { currenUserData } = useAuth();
+  const style = {
+    display: currentUserData.currentGame.question.active ? 'none' : 'block',
+  };
+
+  return (
+    <img style={style} className="game-image main-map" src={Map} alt="mapa" />
+  );
+};
