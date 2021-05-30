c6bfbcfa1c82d01655fd790fe45c848742248dd8
Lenka Verzichová <lenkaverzichova@gmail.com>
2021-05-30 09:56:20 +0200

    Add smoothscroll

--- src/Game/components/Levels/index.jsx
+++ src/Game/components/Levels/index.jsx
@@ -2,9 +2,25 @@ import React, { useEffect } from 'react';
 import { levels } from './levels';
 import { Level } from './../Level';
 import { useAuth } from './../../../Auth/AuthContext';
+import smoothscroll from 'smoothscroll-polyfill';
 
-export const Levels = () => {
+export const Levels = (props) => {
   const { currentUserData } = useAuth();
+
+  useEffect(() => {
+    smoothscroll.polyfill();
+    setTimeout(() => {
+      props.gameRef.current.scroll({
+        left:
+          levels[
+            currentUserData.currentGame.levels.length
+          ].button.style.left.slice(0, -2) -
+          props.gameRef.current.offsetWidth / 2 +
+          35,
+        behavior: 'smooth',
+      });
+    }, 10);
+  }, [currentUserData.currentGame.levels.length, props.gameRef]);
 
   return (
     <>
