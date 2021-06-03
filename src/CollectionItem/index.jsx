8caf8a7dfe741736bd36a87cb57f4b1bf8adf654
Lenka Verzichová <lenkaverzichova@gmail.com>
2021-06-03 22:35:54 +0200

    Beautify CollectionItem

--- src/CollectionItem/index.jsx
+++ src/CollectionItem/index.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useAuth } from './../Auth/AuthContext';
 import { collection } from './../Game/questions';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons';
 import { Audio } from './../Game/components/Audio';
 import './style.css';
 
@@ -39,21 +41,22 @@ export const CollectionItem = () => {
   };
   return item ? (
     <div>
-      <button className="btn btn-back-collection" onClick={handleCollection}>
-        Zpět do kolekce
+      <button
+        className="btn-navigation btn-navigation--collection"
+        onClick={handleCollection}
+      >
+        <FontAwesomeIcon icon={faArrowCircleLeft} size="2x" />
       </button>
+      <h3 className="collection-inner-title">{item.titulek}</h3>
       <div className="collection-inner">
-        <div className="collection-inner__animal">
-          <h4 className="collection-inner__animal-title">{item.titulek}</h4>
-
-          <div
-            className="collection-inner__animal-img"
-            style={{ backgroundImage: `url(${item.obrazek})` }}
-          ></div>
+        <div className="collection-inner__img">
+          <img src={item.obrazek} alt={item.titulek} />
         </div>
-        <div>
+        <div className="collection-inner__information">
           <p>{item.zajimavost}</p>
-          <Audio soubor={item.zvuk} />
+          <div style={{ width: '100%' }}>
+            <Audio soubor={item.zvuk} />
+          </div>
         </div>
       </div>
     </div>
