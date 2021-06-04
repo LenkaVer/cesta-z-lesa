1bf93e2b212259e6a4694a0481bb4ff3d833ba07
Lenka Verzichová <lenkaverzichova@gmail.com>
2021-06-04 22:02:31 +0200

    Improve style of SetUser and Home

--- src/Credits/index.jsx
+++ src/Credits/index.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
+import './style.css';
 
 export const Credits = () => {
   return (
-    <div>
-      <ul>test</ul>
+    <div className="credits">
+      <h3>Zdroje</h3>
+      <nav className="credits__menu">
+        <h4>Zvuky</h4>
+        <ul>
+          <li>
+            <a
+              href="https://www.nasiptaci.info/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              {' '}
+              Naši ptáci
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.myslivost.cz/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              {' '}
+              Myslivost
+            </a>
+          </li>
+        </ul>
+
+        <h4>Obrázky</h4>
+        <ul>
+          <li>
+            <a href="https://pixabay.com/cs/" rel="noreferrer" target="_blank">
+              Pixabay
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.pexels.com/cs-cz/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              {' '}
+              Pexels
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.freepik.com/vectors/background"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Background vector created by pikisuperstar - www.freepik.com
+            </a>
+          </li>
+        </ul>
+        <h4>Poděkování</h4>
+        <ul>
+          <li>
+            <a href="https://czechitas.cz/cs/" rel="noreferrer" target="_blank">
+              Czechitas
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
--- src/SetUser/index.jsx
+++ src/SetUser/index.jsx
@@ -6,16 +6,18 @@ export const SetUser = () => {
   return (
     <>
       <div className="homepage-navigation">
-        <Link className="btn btn-homepage" to="/signup">
-          Registrovat se
-        </Link>
-        <Link className="btn btn-homepage" to="/login">
-          Přihlásit se
-        </Link>
-        <Link className="btn btn-homepage" to="/signup-anonymous">
-          Pokračovat bez registrace
-        </Link>
-        <div className="explanation-user-setup">
+        <div className="homepage-navigation__links">
+          <Link className="btn btn-homepage" to="/signup">
+            Registrovat se
+          </Link>
+          <Link className="btn btn-homepage" to="/login">
+            Přihlásit se
+          </Link>
+          <Link className="btn btn-homepage" to="/signup-anonymous">
+            Pokračovat bez registrace
+          </Link>
+        </div>
+        <div className="homepage-navigation__explanation">
           <p>
             Registrovaní uživatelé si mohou na svém profilu zobrazit dosažené
             body za odehrané hry a mohou se k rozehrané hře vrátit z různých
@@ -26,7 +28,6 @@ export const SetUser = () => {
             zvířeti (obrázek a zajímavost) do své galerie, kterou si mohou na
             profilu prohlížet. Mouhou tak získat galerii 48 zvířat.
           </p>
-          <p></p>
         </div>
       </div>
     </>
